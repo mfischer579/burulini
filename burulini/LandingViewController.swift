a5b730bd485f9eb2d452b749b398b282b020b8a4
Michael Fischer <mfischer579@gmail.com>
2016-09-20 18:51:50 -0500

    Setup commit

---
+++ burulini/LandingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LandingViewController.swift
+//  burulini
+//
+//  Created by Michael Fischer on 9/20/16.
+//  Copyright © 2016 Moose Development. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    
+}
