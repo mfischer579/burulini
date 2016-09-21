ef2efaa8e266b7baf55fc75fcdcf3e6204abd210
Michael Fischer <mfischer579@gmail.com>
2016-09-20 21:56:36 -0500

    Older version of SnapKit - initial setup

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'burulini' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for burulini
+  pod ’SnapKit’, ‘~> 0.22.0’
+
+  target 'buruliniTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'buruliniUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- burulini/LandingViewController.swift
+++ burulini/LandingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class LandingViewController: UIViewController {
     
@@ -17,6 +18,28 @@ class LandingViewController: UIViewController {
     }
     
     func configureView() {
+        view.backgroundColor = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1)
         
+        // Header
+        let topView = UIView()
+        topView.backgroundColor = UIColor(red: 0/255, green: 102/255, blue: 204/255, alpha: 1)
+        view.addSubview(topView)
+        topView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(view)
+            make.width.equalTo(view)
+            make.height.equalTo(70)
+        }
+        
+        // Header Text
+        let headerText = UILabel(frame: CGRectZero)
+        headerText.text = "Welcome to theRegistry"
+        headerText.textColor = UIColor.whiteColor()
+        headerText.textAlignment = .Center
+        topView.addSubview(headerText)
+        headerText.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(topView).offset(-20)
+            make.height.equalTo(20)
+            make.width.equalTo(topView)
+        }
     }
 }
